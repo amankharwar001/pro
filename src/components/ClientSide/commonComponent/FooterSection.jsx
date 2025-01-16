6ce569f59dd957b5fa29a04e35465b9b4d701e63
amankharwar001 <aman.kharwar1311@gmail.com>
2025-01-16 19:05:10 +0530

    logo changes

--- src/components/ClientSide/commonComponent/FooterSection.jsx
+++ src/components/ClientSide/commonComponent/FooterSection.jsx
@@ -9,35 +9,75 @@ import Link from 'next/link';
 
 export default function FooterSection() {
   const [productList, setProductList] = useState(null); // State to store API data
-  
-      
-      
-        useEffect(() => {
-          // Fetch data from API
-          const fetchData = async () => {
-            try {
-              const response = await fetch('/api/public/product/list');
-              if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-              }
-              const result = await response.json();
-              setProductList(result); // Save data to state
-             
-            } catch (err) {
-              console.warn(err.message); // Save error message
-            }
-          };
-      
-          fetchData();
-        }, []);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_PATH;
+
+
+  useEffect(() => {
+    // Fetch data from API
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/public/product/list');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        setProductList(result); // Save data to state
+
+      } catch (err) {
+        console.warn(err.message); // Save error message
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const [logo, setlogo] = useState(); // State to store admin settings
+  const [feviconIcon, setfeviconIcon] = useState(); // State to store avatar images
+
+  console.log("logo show is here",logo)
+
+
+  useEffect(() => {
+    // Function to fetch admin setting data
+    const fetchAdminSetting = async () => {
+      try {
+        const response = await fetch('/api/public/logo');
+        if (response.ok) {
+          const data = await response.json();
+          setlogo(data.logo ||{} ); // Set admin settings or empty object
+          setfeviconIcon(data.fevicon || []); // Set avatar images or empty array
+        } else {
+          setlogo({});
+          setfeviconIcon([]);
+        }
+      } catch (err) {
+        console.error('Error fetching admin settings:', err);
+        setlogo({});
+        setfeviconIcon([]);
+      }
+    };
+
+    fetchAdminSetting(); // Fetch data on component mount
+  }, []); // Empty dependency array ensures it runs only on mount
+
+
   return (
     <div className="relative mt-20">
-        <Footercta/>
+      <Footercta />
       <div className="bg-[#D4D4D4] py-10 pt-48 md:pt-40 -z-10">
         <div className="container m-auto  md:place-content-start  md:text-start grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
           <Slide triggerOnce direction="left">
             <div className="mt-4">
-              <p className="text-base">
+            <Link href={baseUrl} className='flex m-auto'>
+              <Image
+                src={`${baseUrl}${logo?.filePath}`}
+                alt={logo?.altText || 'logo'}
+                className="w-28 m-auto"
+                width={100}
+                height={100}
+              />
+            </Link>
+              <p className="text-base mt-5">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto odio cupiditate corrupti nemo blanditiis veniam
                 voluptates, sunt quaerat possimus quo eligendi reiciendis magnam dicta quos enim quas magni voluptatum nam reprehenderit dignissimos!
               </p>
@@ -73,14 +113,14 @@ export default function FooterSection() {
             <div className="mt-4">
               <h3 className="text-lg font-semibold">Products</h3>
               <ul className="text-muted-foreground flex flex-col gap-5 mt-5">
-              {productList?.map((product) => (
-                                <li
-                                    key={product.id} // Always use a unique key when rendering lists
-                                    className="cursor-pointer hover:text-slate-800"
-                                >
-                                    <Link href={`product/${product?.seo}`}>{product.nickname}</Link>
-                                </li>
-                            ))}
+                {productList?.map((product) => (
+                  <li
+                    key={product.id} // Always use a unique key when rendering lists
+                    className="cursor-pointer hover:text-slate-800"
+                  >
+                    <Link href={`product/${product?.seo}`}>{product.nickname}</Link>
+                  </li>
+                ))}
                 {/* <li>Gift Card</li>
                 <li>Prepaid Gift Card</li>
                 <li>Our job offers</li>
