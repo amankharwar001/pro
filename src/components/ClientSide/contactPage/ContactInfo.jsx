77bd8260233fa463dcd74a14b0b2d62cf4a4596e
amankharwar001 <aman.kharwar1311@gmail.com>
2025-02-10 17:24:36 +0530

    changes complete

--- src/components/ClientSide/contactPage/ContactInfo.jsx
+++ src/components/ClientSide/contactPage/ContactInfo.jsx
@@ -1,86 +1,3 @@
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faLocationDot, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-// import { Fade, Zoom } from 'react-awesome-reveal';
-// import Image from 'next/image';
-
-// function ContactInfo({ data, baseUrl }) {
-//     const svg1 = data?.Images?.find(
-//         (image) => image.referenceId === 2
-//     );
-//     const svg2 = data?.Images?.find(
-//         (image) => image.referenceId === 3
-//     );
-//     const svg3 = data?.Images?.find(
-//         (image) => image.referenceId === 4
-//     );
-
-//     return (
-//         <div className="bg-gray-50 py-12">
-//             <div className="container mx-auto text-center">
-//                 <Fade triggerOnce delay={100}>
-//                     <h2 className="font-bold mb-2">{data?.getInTouchData[0]?.heading || "hello"}</h2>
-//                 </Fade>
-//                 <Fade triggerOnce delay={300}>
-//                     <p className="text-gray-600 mb-8">
-//                         {data?.getInTouchData[0]?.content}
-//                     </p>
-//                 </Fade>
-
-//                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-//                     {/* Head Office Address Card */}
-//                     <div className="bg-white p-8 rounded-lg shadow-md text-center py-12 flex  justify-center">
-//                         <Fade triggerOnce delay={500} direction='up'>
-//                             <div className=''>
-//                                 <Image className='bg-black w-14 h-14 rounded-full p-2 m-auto' src={baseUrl + svg1?.filePath} alt={svg1?.altText} />
-
-//                                 <h3 className="text-lg font-semibold">{data?.getInTouchData[0]?.card[0]?.title}</h3>
-//                                 <p className="text-gray-700 mt-2 text-base md:text-lg">
-//                                     {data.getInTouchData[0]?.card[0]?.content}
-//                                 </p>
-//                             </div>
-//                         </Fade>
-//                     </div>
-
-//                     {/* Email Us Card */}
-//                     <div className="bg-white p-8 rounded-lg shadow-md text-center py-12 flex  justify-center">
-//                         <Fade triggerOnce delay={700} direction='up'>
-//                             <div>
-//                                 <Image className='bg-black w-14 h-14 rounded-full p-2 m-auto' src={baseUrl + svg2.filePath} alt={svg2?.altText} />
-//                                 <h3 className="text-lg font-semibold">{data.getInTouchData[0]?.card[1]?.title}</h3>
-//                                 <p className="text-gray-700 mt-2 text-base md:text-lg">
-//                                     {data.getInTouchData[0]?.card[1]?.content}
-                                    
-//                                 </p>
-//                             </div>
-//                         </Fade>
-//                     </div>
-
-//                     {/* Call Us Card */}
-//                     <div className="bg-white p-8 rounded-lg shadow-md text-center text-base md:text-lg py-12 flex  justify-center">
-//                         <Fade triggerOnce delay={800} direction='up'>
-//                             <div >
-//                                 <Image className='bg-black w-14 h-14 rounded-full p-2 m-auto ' src={baseUrl + svg3?.filePath} alt={svg3?.altText} />
-//                                 <h3 className="text-lg font-semibold">{data.getInTouchData[0]?.card[2]?.title}</h3>
-//                                 <p className="text-gray-700 mt-2">{data.getInTouchData[0]?.card[2]?.content}</p>
-//                             </div>
-//                         </Fade>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default ContactInfo;
-
-
-
-
-
-
-
-
 
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
@@ -144,7 +61,7 @@ function ContactCard({ title, content, imageUrl, altText }) {
       <Fade triggerOnce delay={500} direction="up">
         <div className=''>
           <Image
-            className=" w-14 h-14 rounded-full m-auto bg-black"
+            className=" w-14 h-14 rounded-full object-contain m-auto bg-black"
             src={imageUrl}
             alt={altText}
             width={56}
